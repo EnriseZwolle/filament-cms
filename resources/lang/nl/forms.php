7c688e023c0ee8c89dff1a817701ab3bdc02bf6c
Robin <rsmit@enrise.com>
2025-04-16 17:35:26 +0200

    Title with slug field

---
+++ resources/lang/nl/forms.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'field' => [
+        'edit' => 'Bewerk',
+        'changed' => 'Bijgewerkt',
+        'ok' => 'Ok',
+        'reset' => 'Reset',
+        'cancel' => 'Annuleren ',
+    ],
+];
---
+++ src/Filament/Forms/Fields/SlugInput.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Enrisezwolle\FilamentCms\Filament\Forms\Fields;
+
+use Closure;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Str;
+
+class SlugInput extends TextInput
+{
+    protected string $view = 'filament-cms::components.forms.fields.slug-input';
+
+    protected string|Closure|null $context = null;
+
+    protected bool $cancelled = false;
+
+    protected Closure $recordSlug;
+
+    protected bool|Closure $readOnly = false;
+
+    protected ?Closure $slugInputModelName = null;
+
+    public function slugInputModelName(?Closure $slugInputModelName): static
+    {
+        $this->slugInputModelName = $slugInputModelName;
+
+        return $this;
+    }
+
+    public function getSlugInputModelName(): ?string
+    {
+        return $this->evaluate($this->slugInputModelName);
+    }
+
+    public function slugInputLabelPrefix(?string $labelPrefix): static
+    {
+        $this->labelPrefix = $labelPrefix ?? 'translation - slug: ';
+
+        return $this;
+    }
+
+    public function getLabelPrefix(): string
+    {
+        return $this->evaluate($this->labelPrefix);
+    }
+
+    public function readOnly(Closure|bool $condition = true): static
+    {
+        $this->readOnly = $condition;
+
+        return $this;
+    }
+
+    public function getReadOnly(): bool
+    {
+        return $this->evaluate($this->readOnly);
+    }
+
+    public function slugInputContext(string|Closure|null $context): static
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    public function getContext(): ?string
+    {
+        return $this->evaluate($this->context);
+    }
+
+    public function slugInputRecordSlug(Closure $recordSlug): static
+    {
+        $this->recordSlug = $recordSlug;
+
+        return $this;
+    }
+}
